67c07a09097bc3bff22443e1f47481ec5a71aab9
Bittu Pandit <bittupandit96081@gmail.com>
2026-09-09 09:40:36 +0530

    update

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ contactForm.addEventListener("submit", function (event) {
 
 
     // WhatsApp number
-    const whatsappNumber = "918271734883"; // my WhatsApp number 
+    const whatsappNumber = "+918271734883"; // my WhatsApp number 
 
 
     const whatsappMessage =
